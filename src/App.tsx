819b9713d3426c06dbaeb7e6e85a00c8e33aa479
shrija2005 <shrijabhavani.com>
2024-12-18 15:05:07 +0530

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import MyFirstcom from './components/MyFirstcom'
+
+export default function App() {
+  return (
+    <div>
+      <MyFirstcom></MyFirstcom>
+    </div>
+  )
+}
